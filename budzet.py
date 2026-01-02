56e3ae9d27e108cbd0e839502f1e889b837db55f
NeiRoii <piotrpietrasinski@gmail.com>
2026-01-02 15:29:09 +0100

    Change default budget percentages
    Updated default percentages for budget categories.

--- budzet.py
+++ budzet.py
@@ -52,7 +52,7 @@ COLOR_MAP = {
 }
 
 # Domyślne wartości procentowe
-DEFAULT_PERCENTS = [50, 15, 12, 12, 10, 1]
+DEFAULT_PERCENTS = [50, 15, 10, 10, 10, 5]
 CATEGORIES = list(COLOR_MAP.keys())
 DESCRIPTIONS = [
     "Jedzenie, rachunki, czynsz",
@@ -201,3 +201,4 @@ with col_right:
 st.markdown("---")
 st.caption("Aplikacja stworzona w Pythonie (Streamlit + Plotly)."
            "Stworzone na potrzeby zaliczenia przez Piotr Pietrasińskiego i Oliwię Kowalik")
+
